3960c0e5183b44f6ae5c40497b9f9eb15a20f135
Kushagra Bharti <kushagrabharti@gmail.com>
2024-10-28 17:34:01 -0500

    file routing changes
    pretty minor yk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Home from "\.\\pages\\Home";
+import Projects from "\.\\pages\\Projects";
+import Contact from "\.\\pages\\Contact";
 
 const App: React.FC = () => {
   return (
