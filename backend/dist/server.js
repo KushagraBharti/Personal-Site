e448b89df5eb09e32439a50b64056152f15e5e85
Kushagra Bharti <kushagrabharti@gmail.com>
2024-11-21 09:38:13 -0600

    it should all work now

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,8 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const PORT = 5000;
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
