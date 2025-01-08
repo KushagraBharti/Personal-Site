34437fe49fc0df57dac01111ff0227f9e5038953
Kushagra Bharti <kushagrabharti@gmail.com>
2025-01-09 00:33:12 +0300

    small changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Personal Site Backend is running on http://localhost:${PORT}`);
 });
